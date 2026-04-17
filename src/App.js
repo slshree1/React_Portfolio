f620bab019adf42a8da7315d217b6ab33009d02d
shreyash <shreyashlimbikai123@gmail.com>
2026-04-17 18:48:19 +0530

    fix: replace BrowserRouter with HashRouter for deployment routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
